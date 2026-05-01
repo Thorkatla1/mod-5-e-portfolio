ddd178caf7468baa94bbb98f36c16e098f65cb3a
Thorkatla1 <thorkatla@yahoo.com>
2026-04-30 22:13:59 -0400

    finished 1.11 intergrating emailjs

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
-//template_d7ix2iw
 //service_w5q5a1j
+// //template_d7ix2iw
 //iDH4ulqdbRx8kmuWz
 
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+  emailjs
+    .sendForm(
+      "service_w5q5a1j",
+      "template_d7ix2iw",
+      event.target,
+      "iDH4ulqdbRx8kmuWz",
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable. Please contact me directly on thorkatla@yahoo.com",
+      );
+    });
 
-function contact() {
-  console.log("it works")
-  // event.preventDefault();
-  // emailjs
-  //   .sendForm(
-  //     "service_w5q5a1j",
-  //     "template_d7ix2iw",
-  //     event.target,
-  //     "iDH4ulqdbRx8kmuWz",
-  //   )
-  //   .then(() => {
-  //     console.log("this worked");
-  //   });
-
-
-  // const loading = document.querySelector(".modal__overlay--loading");
-  // const success = document.querySelector(".modal__overlay--success");
-  // loading.classList += " .modal__overlay--visible";
   // setTimeout(() => {
-  //   console.log("it worked");
-  // }, 500);
+  //   console.log("it worked?");
+  // }, 1000);
 }
