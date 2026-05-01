a843a94c61d6795d065c918fffdf057528306fff
Thorkatla1 <thorkatla@yahoo.com>
2026-04-30 21:08:40 -0400

    function contact line 7

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-// template_d7ix2iw
-// service_w5q5a1j
-// iDH4ulqdbRx8kmuWz
-function contact(event){
-    event.preventDefault()
+//template_d7ix2iw
+//service_w5q5a1j
+//iDH4ulqdbRx8kmuWz
+
+
+function contact() {
+  console.log("it works")
+  // event.preventDefault();
+  // emailjs
+  //   .sendForm(
+  //     "service_w5q5a1j",
+  //     "template_d7ix2iw",
+  //     event.target,
+  //     "iDH4ulqdbRx8kmuWz",
+  //   )
+  //   .then(() => {
+  //     console.log("this worked");
+  //   });
+
+
+  // const loading = document.querySelector(".modal__overlay--loading");
+  // const success = document.querySelector(".modal__overlay--success");
+  // loading.classList += " .modal__overlay--visible";
+  // setTimeout(() => {
+  //   console.log("it worked");
+  // }, 500);
 }
